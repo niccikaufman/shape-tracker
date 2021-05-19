0eb0f2893f89189f4d5bac32feaea03e8fd74c5e
Nicci Kaufman <niccikaufman@gmail.com>
2021-05-18 20:35:43 -0700

    fix spacing

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,7 +1,7 @@
 import Triangle from './../src/triangle.js';
 
-describe('Triangle',()=>{
-  test('should correctly create a triangle object with three lengths',()=>{
+describe('Triangle', () => {
+  test('should correctly create a triangle object with three lengths', () => {
     const triangle = new Triangle(2,4,5);
     expect(triangle.side1).toEqual(2);
     expect(triangle.side2).toEqual(4);
@@ -11,7 +11,7 @@ describe('Triangle',()=>{
     const notTriangle = new Triangle(3,9,22);
     expect(notTriangle.checkType()).toEqual("not a triangle");
   });
-  test('should correctly determine whether lengths make a scalene triangle',()=>{
+  test('should correctly determine whether lengths make a scalene triangle', () => {
     const scalTriangle = new Triangle(4,5,7);
     expect(scalTriangle.checkType()).toEqual("scalene triangle");
   });
