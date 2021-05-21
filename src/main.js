28fe1feb1137f745e378f4d9d55955ee78cd3386
Nicci Kaufman <niccikaufman@gmail.com>
2021-05-21 13:52:48 -0700

    fix typo in Rectangle import statement

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Triangle from './js/triangle.js';
-import Rectangle from './rectangle.js';
+import Rectangle from './js/rectangle.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
